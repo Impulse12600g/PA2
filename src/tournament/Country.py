28c4af7438679c73c49a56957d9e1a1c1ccd9526
Impulse12600g <k.lal.1078780@gmail.com>
2022-12-11 15:35:38 -0700

    updates on tournament

---
+++ src/tournament/Country.py
@@ -0,0 +1,10 @@
+
+
+class Country:
+
+    def __init__(self, countryName):
+        self.__countryName = countryName
+
+    def __str__(self):
+        return self.__countryName
+
---
+++ src/tournament/LineUp.py
@@ -0,0 +1,6 @@
+
+
+class LineUp:
+
+    def __init__(self, countryName):
+        self.__countryName = countryName
